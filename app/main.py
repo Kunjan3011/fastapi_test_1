1466bdda2326e737db1b44b7575e22cf04ad20dd
Kunjan3011 <kunjanchittroda3011@gmail.com>
2026-05-05 16:25:57 +0530

    Initialize FastAPI application with database setup and router inclusion

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from app.database import Base, engine
+from app.routers import auth, users
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
+
+app.include_router(users.router)
