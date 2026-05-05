84d9a1056f4152a83b1a77977e6bce3dcc7a4d54
Kunjan3011 <kunjanchittroda3011@gmail.com>
2026-05-05 16:25:25 +0530

    Add database configuration and session management setup

---
+++ app/database.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = 'sqlite:///./tasks.db'
+
+engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+Base = declarative_base()
