0a769e41aa2a308c2315fb5104237730cc206aa5
Kunjan3011 <kunjanchittroda3011@gmail.com>
2026-05-05 16:49:33 +0530

    Add SQLAlchemy models for Users and Tasks (without profile photo and location)

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from app.database import Base
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, index=True, unique=True)
+    hashed_password = Column(String)
+    city = Column(String, index=True)
+    role = Column(String, default="users")
+    # profile_picture=Column(String)
+    # location=Column(String)
+
+
+class Tasks(Base):
+    __tablename__ = 'tasks'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    name = Column(String)
+    description = Column(String)
+    date = Column(String)
