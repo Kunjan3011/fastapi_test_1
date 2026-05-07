ce1395fb6079d10eb64b81506fb2315aa3f9a93a
Kunjan3011 <kunjanchittroda3011@gmail.com>
2026-05-07 18:28:57 +0530

    Add country, location to Users model, Add UserLogs models for logging user activity of login/ register with their IP and location tracking, and Completed Tasks Model properly

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, LargeBinary
+from datetime import *
+
+from sqlalchemy import Column, Integer, String, ForeignKey, LargeBinary, DateTime, Boolean
 
 from app.database import Base
 
@@ -9,11 +11,21 @@ class Users(Base):
     username = Column(String, index=True, unique=True)
     hashed_password = Column(String)
     city = Column(String, index=True)
-    ip = Column(String)
+    country = Column(String)
+    location = Column(String)
     role = Column(String, default="users")
     profile_picture = Column(LargeBinary, nullable=True)
-    profile_file=Column(String)
-    # location=Column(String)
+    profile_file = Column(String)
+
+
+class UserLogs(Base):
+    __tablename__ = 'userlogs'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    ip = Column(String)
+    location = Column(String, nullable=True)
+    activity = Column(String)
+    activity_time = Column(DateTime, default=datetime.now(timezone.utc))
 
 
 class Tasks(Base):
@@ -22,6 +34,8 @@ class Tasks(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     name = Column(String)
     description = Column(String)
+    completed=Column(Boolean,default=False)
+    priority=Column(Integer,default=1)
     created_at = Column(String)
-    updated_at=Column(String,default="Not updated yet")
-    deleted=Column(String,default="False")
+    updated_at = Column(String, default="Not updated yet")
+    deleted = Column(Boolean, default=False)
