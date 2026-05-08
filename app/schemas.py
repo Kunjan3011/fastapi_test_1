45480c528961052c3bff936411b60a47c01ee237
Kunjan3011 <kunjanchittroda3011@gmail.com>
2026-05-08 15:28:58 +0530

    Update the models and schemas with enhanced validation

--- app/schemas.py
+++ app/schemas.py
@@ -2,13 +2,9 @@ from pydantic import BaseModel, Field
 
 
 class UserCreate(BaseModel):
-    username: str
-    password: str
+    username: str = Field(min_length=3)
+    password: str = Field(min_length=8)
     role: str = Field(default="user")
-
-
-class UserUpdate(BaseModel):
-    city: str
 
 
 class UserView(BaseModel):
@@ -24,7 +20,7 @@ class TaskView(BaseModel):
     name: str
     description: str
     priority: int
-    completed: bool
+    completed: str
     created_at: str
     updated_at: str = Field(default="Not updated yet")
 
@@ -35,7 +31,7 @@ class Token(BaseModel):
 
 
 class TaskCreate(BaseModel):
-    name: str
-    description: str
-    priority: int
-    completed: bool = Field(default=False)
+    name: str = Field(default="Task")
+    description: str = Field(default="None")
+    priority: int = Field(gt=0, lt=6, default=1)
+    completed: str = Field(default="no")
