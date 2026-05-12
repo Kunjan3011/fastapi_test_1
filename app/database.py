6778b3c38db76585223736d8ee681069c0283667
Kunjan3011 <kunjanchittroda3011@gmail.com>
2026-05-12 15:50:01 +0530

    Changes in database.py for deployment

--- app/database.py
+++ app/database.py
@@ -4,13 +4,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#DATABASE_URL = 'postgresql://postgres:root@localhost/tasks'
 
 load_dotenv()
 #make sure you have database named tasks in pgAdmin4
-DATABASE_URL=os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+
+if "localhost" in DATABASE_URL:
+    engine = create_engine(DATABASE_URL)
+else:
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True,
+        connect_args={"sslmode": "require"}
+    )
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
